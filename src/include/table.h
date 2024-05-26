fecac2f2b074ff6544a43ca78378aec54b458174
chunemeng <chunemeng@outlook.com>
2024-05-26 23:35:57 +0800

    Optimize merge

--- src/include/table.h
+++ src/include/table.h
@@ -11,252 +11,279 @@
 #include <iostream>
 
 namespace LSMKV {
-    //instance of sst
-    class Table {
-    public:
-        explicit Table(const uint64_t &file_no) : file_no(file_no) {
-        }
+	//instance of sst
+	class Table {
+	 public:
+		Table() {
+			this->timestamp = 0;
+			sst.emplace_back(UINT64_MAX, Slice());
+		}
+		explicit Table(const uint64_t& file_no) : file_no(file_no) {
+		}
 
-        char *reserve(size_t size) {
-            return arena.allocate(size);
-        }
+		char* reserve(size_t size) {
+			return arena.allocate(size);
+		}
 
-        explicit Table(const char *tmp, const uint64_t &file_no) {
-            timestamp = DecodeFixed64(tmp);
-            this->file_no = file_no;
-            uint64_t size = DecodeFixed64(tmp + 8);
-            sst.reserve(size);
-            char *buf;
-            if ((isFilter = LSMKV::Option::isFilter)) {
-                buf = arena.allocate(size * 20 + bloom_size);
-                bloom = Slice(buf, bloom_size);
-            } else {
-                buf = arena.allocate(size * 20);
-            }
+		explicit Table(const char* tmp, const uint64_t& file_no) {
+			timestamp = DecodeFixed64(tmp);
+			this->file_no = file_no;
+			uint64_t size = DecodeFixed64(tmp + 8);
+			sst.reserve(size);
+			char* buf;
+			if ((isFilter = LSMKV::Option::isFilter)) {
+				buf = arena.allocate(size * 20 + bloom_size);
+				bloom = Slice(buf, bloom_size);
+			}
+			else {
+				buf = arena.allocate(size * 20);
+			}
 
-            // NO NEED TO COPY HEADER
-            // IF OPEN FILTER COPY IT
-            memcpy(buf, tmp + 32 + !isFilter * bloom_size, (isFilter ? bloom_size : 0) + size * 20);
-            uint64_t offset = isFilter * bloom_size;
+			// NO NEED TO COPY HEADER
+			// IF OPEN FILTER COPY IT
+			memcpy(buf, tmp + 32 + !isFilter * bloom_size, (isFilter ? bloom_size : 0) + size * 20);
+			uint64_t offset = isFilter * bloom_size;
 
-            // TODO MAY NEED COMPARE!!
-            for (uint64_t i = 0; i < size * 20; i += 20) {
-                sst.emplace_back(DecodeFixed64(buf + offset + i), Slice(buf + offset + 8 + i, 12));
-            }
+			// TODO MAY NEED COMPARE!!
+			for (uint64_t i = 0; i < size * 20; i += 20) {
+				sst.emplace_back(DecodeFixed64(buf + offset + i), Slice(buf + offset + 8 + i, 12));
+			}
 
-        }
+		}
 
-        void pushCache(const char *tmp) {
-            if ((isFilter = LSMKV::Option::isFilter)) {
-                bloom = Slice(tmp + 32, bloom_size);
-            }
-            timestamp = DecodeFixed64(tmp);
-            uint64_t size = DecodeFixed64(tmp + 8);
-            uint64_t offset = bloom_size + 32;
+		void pushCache(const char* tmp) {
+			if ((isFilter = LSMKV::Option::isFilter)) {
+				bloom = Slice(tmp + 32, bloom_size);
+			}
+			timestamp = DecodeFixed64(tmp);
+			uint64_t size = DecodeFixed64(tmp + 8);
+			uint64_t offset = bloom_size + 32;
 
-            // TODO MAY NEED COMPARE!!
-            sst.reserve(size);
-            for (uint64_t i = 0; i < size * 20; i += 20) {
-                sst.emplace_back(DecodeFixed64(tmp + offset + i), Slice(tmp + offset + 8 + i, 12));
-            }
-        }
+			// TODO MAY NEED COMPARE!!
+			sst.reserve(size);
+			for (uint64_t i = 0; i < size * 20; i += 20) {
+				sst.emplace_back(DecodeFixed64(tmp + offset + i), Slice(tmp + offset + 8 + i, 12));
+			}
+		}
 
-        [[nodiscard]] uint64_t GetTimestamp() const {
-            return timestamp;
-        }
+		[[nodiscard]] uint64_t GetTimestamp() const {
+			return timestamp;
+		}
 
-        [[nodiscard]] bool KeyMatch(const uint64_t &key) const {
-            if (key < sst[0].first || key > sst[sst.size() - 1].first) {
-                return false;
-            }
-            return !isFilter || KeyMayMatch(key, bloom);
-        }
+		[[nodiscard]] bool KeyMatch(const uint64_t& key) const {
+			if (key < sst[0].first || key > sst[sst.size() - 1].first) {
+				return false;
+			}
+			return !isFilter || KeyMayMatch(key, bloom);
+		}
 
-        ~Table() = default;
+		~Table() = default;
 
-        bool operator<(const Table &rhs) const {
-            return timestamp < rhs.timestamp;
-        }
+		bool operator<(const Table& rhs) const {
+			return timestamp < rhs.timestamp;
+		}
 
-        bool operator<(const uint64_t &rhs) const {
-            return timestamp < rhs;
-        }
+		bool operator<(const uint64_t& rhs) const {
+			return timestamp < rhs;
+		}
 
-        std::string get(const uint64_t &key) {
-            if (!KeyMatch(key)) {
-                return "";
-            }
-            uint64_t offset = BinarySearchGet(key);
-            if (offset >= sst.size()) {
-                return "";
-            } else {
-                return {sst[offset].second.data(), sst[offset].second.size()};
-            }
-        }
+		std::string get(const uint64_t& key) {
+			if (!KeyMatch(key)) {
+				return "";
+			}
+			uint64_t offset = BinarySearchGet(key);
+			if (offset >= sst.size()) {
+				return "";
+			}
+			else {
+				return { sst[offset].second.data(), sst[offset].second.size() };
+			}
+		}
 
-    private:
-        [[nodiscard]] uint64_t BinarySearchGet(const uint64_t &key) const {
-            // TODO 斐波那契分割优化(?)
-            // NOT INCLUDE END
-            uint64_t left = 0, right = sst.size(), mid;
-            while (left < right) {
-                mid = left + ((right - left) >> 1);
-                if (sst[mid].first < key)
-                    left = mid + 1;
-                else
-                    right = mid;
-            }
-            // To avoid False positive
-            if (left <= sst.size() && sst[left].first == key) {
-                return left;
-            }
+	 private:
+		[[nodiscard]] uint64_t BinarySearchGet(const uint64_t& key) const {
+			// TODO 斐波那契分割优化(?)
+			// NOT INCLUDE END
+			uint64_t left = 0, right = sst.size(), mid;
+			while (left < right) {
+				mid = left + ((right - left) >> 1);
+				if (sst[mid].first < key)
+					left = mid + 1;
+				else
+					right = mid;
+			}
+			// To avoid False positive
+			if (left <= sst.size() && sst[left].first == key) {
+				return left;
+			}
 
-            return sst.size();
-//			assert(start < end && start < sst.size() && end < sst.size());
-//			uint64_t low = start + 1, high = end - 1, mid;
-//			if (key == sst[start].first) {
-//				return start;
-//			}
-//			while (low <= high) {
-//				mid = low + ((high - low) >> 1);
-//				if (key < sst[mid].first)
-//					high = mid - 1;
-//				else if (key > sst[mid].first)
-//					low = mid + 1;
-//				else
-//					return mid;
-//			}
-//			return sst.size();
-        }
+			return sst.size();
+			//			assert(start < end && start < sst.size() && end < sst.size());
+			//			uint64_t low = start + 1, high = end - 1, mid;
+			//			if (key == sst[start].first) {
+			//				return start;
+			//			}
+			//			while (low <= high) {
+			//				mid = low + ((high - low) >> 1);
+			//				if (key < sst[mid].first)
+			//					high = mid - 1;
+			//				else if (key > sst[mid].first)
+			//					low = mid + 1;
+			//				else
+			//					return mid;
+			//			}
+			//			return sst.size();
+		}
 
-        [[nodiscard]] uint64_t BinarySearchLocation(const uint64_t &start,
-                                                    const uint64_t &end,
-                                                    const uint64_t &key) const {
-            // TODO 斐波那契分割优化(?)
-            // NOT INCLUDE END
-            uint64_t left = start, right = end, mid;
-            while (left < right) {
-                mid = left + ((right - left) >> 1);
-                if (sst[mid].first < key)
-                    left = mid + 1;
-                else
-                    right = mid;
-            }
-            return left;
-        }
+		[[nodiscard]] uint64_t BinarySearchLocation(const uint64_t& start,
+			const uint64_t& end,
+			const uint64_t& key) const {
+			// TODO 斐波那契分割优化(?)
+			// NOT INCLUDE END
+			uint64_t left = start, right = end, mid;
+			while (left < right) {
+				mid = left + ((right - left) >> 1);
+				if (sst[mid].first < key)
+					left = mid + 1;
+				else
+					right = mid;
+			}
+			return left;
+		}
 
-        bool isFilter = true;
-        Arena arena;
-        std::vector<std::pair<uint64_t, Slice>> sst;
-        Slice bloom;
-        uint64_t file_no = 0;
-        uint64_t timestamp = 0;
-    public:
-        class TableIterator {
-        public:
-            explicit TableIterator(Table *table) : _table(table) {
-            };
+		bool isFilter = true;
+		Arena arena;
+		std::vector<std::pair<uint64_t, Slice>> sst;
+		Slice bloom;
+		uint64_t file_no = 0;
+		uint64_t timestamp = 0;
+	 public:
+		class TableIterator {
+		 public:
+			explicit TableIterator(Table* table) : _table(table) {
+			};
 
-            ~TableIterator() {
-                delete _table;
-                _table = nullptr;
-            }
+			void seekToEmpty() {
+				setTimestamp(0);
+				_end = 0;
+			}
 
-            [[nodiscard]] uint64_t size() const {
-                return _table->sst.size();
-            }
+			~TableIterator() {
+				delete _table;
+				_table = nullptr;
+			}
 
-            [[nodiscard]] bool hasNext() const {
-                return _end > _cur;
-            }
+			[[nodiscard]] uint64_t size() const {
+				return _table->sst.size();
+			}
 
-            [[nodiscard]] Slice value() const {
-                return _table->sst[_cur].second;
-            }
+			[[nodiscard]] bool hasNext() const {
+				return _end > _cur;
+			}
 
-            [[nodiscard]] uint64_t key() const {
-                return _table->sst[_cur].first;
-            }
+			[[nodiscard]] Slice value() const {
+				return _table->sst[_cur].second;
+			}
 
-            [[nodiscard]] bool AtStart() const {
-                return _cur == 0;
-            }
+			[[nodiscard]] uint64_t merge_key() {
+				if (_cur < _end) [[likely]] {
+					return _table->sst[_cur].first;
+				} else [[unlikely]] {
+					return UINT64_MAX;
+				}
+			}
 
-            void next() {
-                _cur++;
-            }
+			void merge_next() {
+				if (_cur == _end) [[unlikely]] {
+					seekToEmpty();
+				}
+				_cur++;
+			}
 
-            void seek(uint64_t key) {
-                if (!_table->KeyMatch(key)) {
-                    _end = 0;
-                    _cur = 1;
-                    return;
-                }
-                _cur = _table->BinarySearchGet(key);
-                _end = _table->sst.size();
-            }
+			[[nodiscard]] uint64_t key() const {
+				return _table->sst[_cur].first;
+			}
 
-            void seek(const uint64_t &K1, const uint64_t &K2) {
-                if (!InRange(K1, K2)) {
-                    _cur = _end + 1;
-                }
-                _end = _table->sst.size();
-                _cur = _table->BinarySearchLocation(0, _end, K1);
-                _end = _table->BinarySearchLocation(_cur, _end, K2);
-                if (_end < _table->sst.size() && _table->sst[_end].first == K2) {
-                    _end++;
-                }
-            }
+			[[nodiscard]] bool AtStart() const {
+				return _cur == 0;
+			}
 
-            void seekToLast() {
-                _end = _table->sst.size();
-                _cur = _end - 1;
-            }
 
-            [[nodiscard]] uint64_t LargestKey() const {
-                return _table->sst[_table->sst.size() - 1].first;
-            }
+			void next() {
+				_cur++;
+			}
 
-            [[nodiscard]] uint64_t SmallestKey() const {
-                return _table->sst[0].first;
-            }
+			void seek(uint64_t key) {
+				if (!_table->KeyMatch(key)) {
+					_end = 0;
+					_cur = 1;
+					return;
+				}
+				_cur = _table->BinarySearchGet(key);
+				_end = _table->sst.size();
+			}
 
-            void setFileNo(const uint64_t &file_no) {
-                _table->file_no = file_no;
-            }
+			void seek(const uint64_t& K1, const uint64_t& K2) {
+				if (!InRange(K1, K2)) {
+					_cur = _end + 1;
+				}
+				_end = _table->sst.size();
+				_cur = _table->BinarySearchLocation(0, _end, K1);
+				_end = _table->BinarySearchLocation(_cur, _end, K2);
+				if (_end < _table->sst.size() && _table->sst[_end].first == K2) {
+					_end++;
+				}
+			}
 
-            void setTimestamp(const uint64_t &timestamp) {
-                _table->timestamp = timestamp;
-            }
+			void seekToLast() {
+				_end = _table->sst.size();
+				_cur = _end - 1;
+			}
 
-            [[nodiscard]] bool InRange(const uint64_t &smallest, const uint64_t &largest) const {
-                return std::max(smallest, SmallestKey()) <= std::min(largest, LargestKey());
-            }
+			[[nodiscard]] uint64_t LargestKey() const {
+				return _table->sst[_table->sst.size() - 1].first;
+			}
 
-            void seekToFirst() {
-                _cur = 0;
-                _end = _table->sst.size();
-            }
+			[[nodiscard]] uint64_t SmallestKey() const {
+				return _table->sst[0].first;
+			}
 
-            [[nodiscard]] uint64_t timestamp() const {
-                return _table->timestamp;
-            }
+			void setFileNo(const uint64_t& file_no) {
+				_table->file_no = file_no;
+			}
 
-            [[nodiscard]] uint64_t file_no() const {
-                return _table->file_no;
-            }
+			void setTimestamp(const uint64_t& timestamp) {
+				_table->timestamp = timestamp;
+			}
 
-        private:
-            Table *_table;
-            // not include _end!!!!
-            uint64_t _cur{};
-            uint64_t _end{};
-        };
+			[[nodiscard]] bool InRange(const uint64_t& smallest, const uint64_t& largest) const {
+				return std::max(smallest, SmallestKey()) <= std::min(largest, LargestKey());
+			}
 
-        TableIterator *Iterator() {
-            return new Table::TableIterator(this);
-        }
-    };
+			void seekToFirst() {
+				_cur = 0;
+				_end = _table->sst.size();
+			}
+
+			[[nodiscard]] uint64_t timestamp() const {
+				return _table->timestamp;
+			}
+
+			[[nodiscard]] uint64_t file_no() const {
+				return _table->file_no;
+			}
+
+		 private:
+			Table* _table;
+			// not include _end!!!!
+			uint64_t _cur{};
+			uint64_t _end{};
+		};
+
+		TableIterator* Iterator() {
+			return new Table::TableIterator(this);
+		}
+	};
 
 }
 
